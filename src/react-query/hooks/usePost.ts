1f1b68268eccf638749739f5c863c25a1823c95c
mkg-coding <marcgoellnitzcoding@gmail.com>
2025-10-17 23:50:59 +0200

    Paginated Queries

--- src/react-query/hooks/usePost.ts
+++ src/react-query/hooks/usePost.ts
@@ -8,18 +8,27 @@ export interface Post {
   userId: number;
 }
 
-const usePost = (userId: number | undefined) => {
+interface PostQuery {
+  page: number;
+  pageSize: number;
+}
+
+const usePost = (query: PostQuery) => {
   const fetchPosts = () =>
     axios
       .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
-        params: { userId },
+        params: {
+          _start: (query.page - 1) * query.pageSize,
+          _limit: query.pageSize,
+        },
       })
       .then((res) => res.data);
 
   return useQuery<Post[], Error>({
-    queryKey: userId ? ["users", userId, "posts"] : ["posts"],
+    queryKey: ["posts", query],
     queryFn: fetchPosts,
     staleTime: 10 * 1000,
+    keepPreviousData: true,
   });
 };
 
